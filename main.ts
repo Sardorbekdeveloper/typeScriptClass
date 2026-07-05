67e189c6959250265f8b5e863b11d70ba2ee31e2
Sardorbekdeveloper <davletboyevsardorbek08@gmail.com>
2026-07-05 17:28:05 +0500

    1-task

--- main.ts
+++ main.ts
@@ -1,6 +1,23 @@
 class Car {
+
     model: string
     year: number
     color: string
-}
 
+    constructor(model: string, year: number) {
+        this.model = model
+        this.year = year
+        this.color = "White"
+    }
+
+    changeColor(newColor: string): void {
+        this.color = newColor
+    }
+
+    displayInfo(): void {
+        console.log(`Model: ${this.model}`)
+        console.log(`Year: ${this.year}`)
+        console.log(`Color: ${this.color}`)
+    }
+
+}
